e210aeadd953aeddeba0b3fe3e42b88615f401c3
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-02-23 22:44:23 -0500

    Creating Dao Class

---
+++ app/src/main/java/com/example/mvvmarchitecture/NoteDatabase.java
@@ -0,0 +1,10 @@
+package com.example.mvvmarchitecture;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Note.class}, version = 1)
+public abstract class NoteDatabase extends RoomDatabase {
+}
+
+
