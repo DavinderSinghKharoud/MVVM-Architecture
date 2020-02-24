a33d5bcc43e45766e3cf7dca2f87ad9b67278c86
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-02-23 22:51:57 -0500

    Creating Database extending Room Database

--- app/src/main/java/com/example/mvvmarchitecture/NoteDatabase.java
+++ app/src/main/java/com/example/mvvmarchitecture/NoteDatabase.java
@@ -1,10 +1,29 @@
 package com.example.mvvmarchitecture;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+//Singleton Pattern
 @Database(entities = {Note.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
+
+    private static NoteDatabase instance;
+
+    public abstract NoteDao noteDao();
+
+    public static synchronized NoteDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    NoteDatabase.class, "note_database")
+                    .fallbackToDestructiveMigration()              //If we increment the database we just start with the new database.
+                    .build();
+        }
+
+        return instance;
+    }
 }
 
 
