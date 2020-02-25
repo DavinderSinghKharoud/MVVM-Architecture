fb04349c8a78bc4b2c89b863f31f7870f022990e
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-02-24 22:07:35 -0500

    Adding view model to the mainActivity.

--- app/src/main/java/com/example/mvvmarchitecture/MainActivity.java
+++ app/src/main/java/com/example/mvvmarchitecture/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.mvvmarchitecture;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+
+        //this will be triggered every time data changes
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                //update Recyclerview
+                Toast.makeText(MainActivity.this,"On changed",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
